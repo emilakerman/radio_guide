ae8aeb7b4d4f076c7b850b576d142dee565b1d85
emilakerman <emil.akerman@iths.se>
2023-10-30 12:43:31 +0100

    added static colors

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xffE9FDB3);
+  static const secondary = Color(0xffFF6666);
+  static const complement = Color(0xffC2FF3F);
+  static const additional = Color(0xff1E4538);
+  static const white = Colors.white;
+  static const black = Colors.black;
+}
