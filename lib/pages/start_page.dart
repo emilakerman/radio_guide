9c7568c94c27b7628e92a4cb7ddeeb7a1c820779
emilakerman <emil.akerman@iths.se>
2023-11-07 10:24:10 +0100

    completed start page design

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -14,6 +14,7 @@ class StartPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
+            SizedBox.shrink(),
             Container(
               color: AppColors.secondary,
               child: const Image(
@@ -47,25 +48,63 @@ class StartPage extends StatelessWidget {
                 ],
               ),
             ),
-            const Icon(Icons.arrow_downward),
+            _buildIconCollection(),
             Container(
               padding: const EdgeInsets.symmetric(horizontal: 25),
               width: double.infinity,
               height: 50,
               child: ElevatedButton(
                 onPressed: () => context.goNamed(AppRoutes.channels.name),
-                child: const Text("Lets go"),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: AppColors.additional,
                   elevation: 4,
                   shadowColor: AppColors.black,
                   shape: const StadiumBorder(),
                 ),
+                child: const Text("Lets go"),
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildIconCollection() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        const Image(width: 90, height: 90, image: AssetImage("assets/images/arrow_down.png")),
+        Column(
+          children: [
+            _buildStarIcon(color: AppColors.additional),
+            const SizedBox(height: 80),
+            _buildStarIcon(color: AppColors.secondary),
+          ],
+        ),
+        Column(
+          children: [
+            _buildStarIcon(color: AppColors.secondary),
+            const SizedBox(height: 18),
+            _buildStarIcon(color: AppColors.complement),
+            const SizedBox(height: 5),
+            _buildStarIcon(color: AppColors.additional),
+          ],
+        ),
+        Column(
+          children: [
+            const SizedBox(height: 5),
+            _buildStarIcon(color: AppColors.complement),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Icon _buildStarIcon({required Color? color}) {
+    return Icon(
+      Icons.star,
+      color: color,
     );
   }
 }
